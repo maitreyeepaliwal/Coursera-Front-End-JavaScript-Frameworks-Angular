f0ec2277006faa09d07a958af5684b73e9eb670c
Maitreyee Paliwal <maitreyeepaliwal@gmail.com>
2022-01-06 17:00:12 +0530

    'Components2'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -24,6 +28,9 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
