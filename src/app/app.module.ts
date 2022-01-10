89be8842580c8c05f7a8a047bb177c4c7e79195e
Maitreyee Paliwal <maitreyeepaliwal@gmail.com>
2022-01-11 00:09:34 +0530

    Reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,13 @@ import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './approuting/approuting.module';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -42,11 +49,18 @@ import { MatDialogModule } from '@angular/material/dialog';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
     MatListModule,
     MatGridListModule,
     MatDialogModule,
     MatCardModule,
     MatButtonModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule 
   ],
   providers: [DishService, PromotionService, LeaderService],
   entryComponents: [
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {MatDialog, MatDialogRef} from '@angular/material';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,16 @@ import {MatDialog, MatDialogRef} from '@angular/material';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user = {username: '', password: '', remember: false};
+
+  constructor(public dialogRef: MatDialogRef<LoginComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log('User: ', this.user);
+    this.dialogRef.close();
   }
 
 }
